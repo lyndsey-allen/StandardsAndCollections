849a433af66dd1b220e739febfa018703c1b92dd
Lyndsey Allen <lyndsey.allen1@northumbria-healthcare.nhs.uk>
2026-02-17 15:14:55 +0000

    init commit

---
+++ monitor_standards.r
@@ -0,0 +1,55 @@
+
+# ---- NHS Standards & Collections Monitor ----
+
+library(rvest)
+library(dplyr)
+library(readr)
+library(digest)
+library(stringr)
+
+url <- "https://digital.nhs.uk/data-and-information/information-standards/governance/latest-activity/standards-and-collections"
+
+# ---- Scrape page ----
+page <- read_html(url)
+
+# Extract all tables (Approved + Current + Draft)
+tables <- page |> html_elements("table")
+
+data_list <- lapply(tables, html_table)
+
+data <- bind_rows(data_list)
+
+# Clean column names
+names(data) <- make.names(names(data))
+
+# Remove empty rows
+data <- data |> filter(if_any(everything(), ~ !is.na(.)))
+
+# ---- Create fingerprint ----
+current_hash <- digest(data, algo = "md5")
+
+# ---- Load previous hash ----
+hash_file <- "data/last_hash.txt"
+snapshot_file <- "data/last_snapshot.csv"
+
+if (file.exists(hash_file)) {
+  old_hash <- readLines(hash_file)
+} else {
+  old_hash <- ""
+}
+
+# ---- Compare ----
+changed <- current_hash != old_hash
+
+cat("Changed:", changed, "\n")
+
+# ---- Save new state ----
+writeLines(current_hash, hash_file)
+write_csv(data, snapshot_file)
+
+# ---- Create flag file for GitHub Actions ----
+if (changed) {
+  writeLines("changed", "data/change_flag.txt")
+} else {
+  writeLines("no_change", "data/change_flag.txt")
+}
